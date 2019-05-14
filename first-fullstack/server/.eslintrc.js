9ed9355c2cff4aa573288032a35f555df7f6fc04
rashaun.ellison <rashaun.ellison@sharecare.com>
2019-05-14 14:18:03 -0400

    set up for server code, allowing console.log statements w/ eslint

---
+++ first-fullstack/server/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "vue"
+    ],
+    // "options":{
+         
+    // },
+    "rules": {
+       "no-console": ["error", { allow: ["log", "error"] }]
+    }
+};

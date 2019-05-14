1e3c9f1b4fa532b5f2ada75878fd7c97c0af8fcf
rashaun.ellison <rashaun.ellison@sharecare.com>
2019-05-14 16:07:04 -0400

    server-side boiler plate and npm start command working

---
+++ server/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "no-console": ["error", { allow: ["log", "error"] }]
+    }
+};
